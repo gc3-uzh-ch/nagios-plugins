65f5287849c2fbff44e52f03910c0c830ac3e596
Francesco De Giorgi <deggio@gw2.gc3.uzh.ch>
2014-11-17 17:19:17 +0100

    check_dothill_ctrl now checks a single dothill ctrl

--- lustre/check_dothill_ctrl.py
+++ lustre/check_dothill_ctrl.py
@@ -19,6 +19,94 @@ from optparse import OptionParser
 import yaml
 
 
+yaml_document = """
+## YAML conf for check_controller.py script
+## refer to README in the same directory for a quickstart
+
+# to authenticate on the controllers' management (using 'manage' account)
+authentication:
+ md5_hex_hash: 539e12f63b693a9970a97b885e857f8b
+
+# on those hosts, query the status of the following objects
+objects:
+ controllers:
+ # on this object, look for this attribute
+  durable-id:
+   # this attribute could have the following texts
+   - controller_a
+   - controller_b
+#   - expport_out1_a0
+#   - expport_out1_b0
+   - mgmtport_a
+   - mgmtport_b
+   - hostport_A0   
+   - hostport_A2   
+   - hostport_B0   
+   - hostport_B2
+   - Ctlr B CF
+   - Ctlr A CF
+
+ 
+ volumes:
+ # on this object, look for this attribute
+  volume-name:
+  # this attribute may have the following texts
+   - ost01
+   - ost02
+   - ost03
+   - ost04       
+   - ost05
+   - ost06
+   - ost07
+   - ost08       
+   - ost09
+   - ost10
+   - ost11
+   - ost12       
+   - ost13
+   - ost14
+   - ost15
+   - ost16       
+   - ost17
+   - ost18
+   - ost19
+   - ost20       
+   - ost21
+   - ost22
+   - ost23
+   - ost24       
+   - ost25
+   - ost26
+   - ost27
+   - ost28       
+   - ost29
+   - ost30
+   - ost31
+   - ost32
+
+
+# define some hosts
+hosts:
+ # this is not a name on the dns or /etc/hosts
+ sclustre-ctrl-oss1-oss2:
+  # controller A and B, with IPs
+  - sclustre-ctrl-oss1-oss2-a.es: 10.128.93.16
+  - sclustre-ctrl-oss1-oss2-b.es: 10.128.93.17
+ sclustre-ctrl-oss3-oss4:
+  - sclustre-ctrl-oss3-oss4-a.es: 10.128.93.20
+  - sclustre-ctrl-oss3-oss4-b.es: 10.128.93.21 
+ sclustre-ctrl-oss5-oss6:
+  - sclustre-ctrl-oss5-oss6-a.es: 10.128.93.24
+  - sclustre-ctrl-oss5-oss6-b.es: 10.128.93.25 
+ sclustre-ctrl-oss7-oss8:
+  - sclustre-ctrl-oss7-oss8-a.es: 10.128.93.28
+  - sclustre-ctrl-oss7-oss8-b.es: 10.128.93.29 
+ sclustre-ctrl-mds1-mds2:
+  - sclustre-ctrl-mds1-mds2-a.es: 10.128.93.12
+  - sclustre-ctrl-mds1-mds2-b.es: 10.128.93.13  
+"""
+
+
 ## print a list of string in a nice way
 def get_nice_string(list_or_iterator):
     return ", ".join( str(x) for x in list_or_iterator)
@@ -85,7 +173,7 @@ class ExitStatement:
 	def exit_statement(self):
 		for component in self.component_list:
 			string = component.get_component_exit()
-			self.string_list.append("on " + component.host + ": " + string)
+			self.string_list.append("on " + component.ctrl + ": " + string)
 		return self.final_status + " -- " + get_nice_string(self.string_list)	
 		
 	def exit_code(self):
@@ -95,12 +183,13 @@ class ExitStatement:
 class Component:
 	""" A component is to be intended a 'volume' or 'controller' """
 	
-	def __init__(self, component_name, check_dict, host):
+	def __init__(self, component_name, check_dict, host, requested_ctrl):
 		self.name = component_name
 		self.dict = check_dict
 		self.element_list = dict()
 		self.host = host
 		self.status_message = []
+		self.ctrl = requested_ctrl
 		self.__status = 'OK'
 		
 	### CHECK element in component is ok, parse XML and get the health of each element
@@ -139,39 +228,27 @@ class Component:
 
 #	
 
-class Parse_YAML:
-	""" Useful to parse YAML configuration file and return reference to this file """
-	
-	def __init__(self, filename):
-		try:
-			with open(filename, 'r') as f:
-			    self.doc = yaml.load(f)
-		except IOError:
-			print "ioerror"
-	
-	def get_conf(self):
-		return self.doc
-
-
 
 '''
 start main
 configure optionparser
 read arguments from command line
 '''
-parser = OptionParser("Usage: %prog -c conf_filename")
-parser.add_option("-c", "--config", dest="conf_filename",
-	default='', help="Configuration file")
+parser = OptionParser("Usage: %prog -c conf_filename -H dothill_ctrl")
+parser.add_option("-H", "--host", dest="dothill_ctrl",
+	default='', help="IP address of DotHill storage controller management")	
 
 (options, args) = parser.parse_args()
 
-if not options.conf_filename:
+# input sanit
+if not options.dothill_ctrl:
 	parser.print_help()
-	sys.exit(2)
+	sys.exit(2)	
 
-# parse YAML and get reference to configuration file
-conf = Parse_YAML(options.conf_filename)
-doc = conf.get_conf()
+# load the inline yaml configuration
+doc = yaml.load(yaml_document)
+# this is the controller we want to check
+requested_ctrl = options.dothill_ctrl
 
 # create an exit statement, to be filled at the end of the job
 exit_stm = ExitStatement()
@@ -191,11 +268,14 @@ for obj in doc['objects']:
 				break
 				
 			# iterate over different way to reach the same storage array (break loop if success)
-			for ip in ctrl_name:
-				component = Component(obj, check_dict, host)
-				r = http_connection.http_get(ctrl_name[ip], obj)
+			for ctrl in ctrl_name:
+				# if not the ctrl we want, please break to the next
+				if ctrl != requested_ctrl:
+					break
+				component = Component(obj, check_dict, host, requested_ctrl)
+				r = http_connection.http_get(ctrl_name[ctrl], obj)
 				if not r:
-					component.add_custom_status("Unable to contact " + ip + " (" + ctrl_name[ip] + ")")
+					component.add_custom_status("Unable to contact " + ctrl + " (" + ctrl_name[ctrl] + ")")
 					exit_stm.add_component_status(component)			
 					continue
 				XMLdoc = etree.parse(r.raw)
@@ -212,4 +292,5 @@ print exit_stm.exit_statement()
 
 sys.exit(exit_stm.exit_code())
 		
+
 
